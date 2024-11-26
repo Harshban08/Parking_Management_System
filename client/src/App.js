fa185e41992ffa032d771e0141d67e9f3ce90fc7
Harsh_Bansal <harshbansal49177@gmail.com>
2024-11-26 11:57:10 +0530

    much more

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
-import ParkingSpace from './Components/ParkingSpace';
 import Login from './Components/Login';
 import SignUp from './Components/SignUp';
+import Profile from './Components/Profile';
+import Footer from './Components/Footer';
+import ParkingSpace from './Components/ParkingSpace';
+import Wallet from './Components/Wallet'; 
+import Contact from './Components/Contact';  
+import Bookings from './Components/Bookings';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Login/>
-      <ParkingSpace/>
-      <SignUp/>
-      <Footer/>
+    <div className='App'>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ParkingSpace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/parking-space" element={<ParkingSpace />} />
+        <Route path="/wallet" element={<Wallet />} />  {/* Add Wallet route */}
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/bookings' element={<Bookings />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
